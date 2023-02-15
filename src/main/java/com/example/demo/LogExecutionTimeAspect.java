8af6e56e46d5947f6d53e185f90e35f6601c381d
tiendainguyen <daint.dev@gmail.com>
2023-02-15 09:34:29 +0700

    update: base

---
+++ src/main/java/com/example/demo/LogExecutionTimeAspect.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogExecutionTimeAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogExecutionTimeAspect.class);
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        LOGGER.info("Execution time of {}: {} ms", joinPoint.getSignature(), executionTime);
+        return proceed;
+    }
+}
